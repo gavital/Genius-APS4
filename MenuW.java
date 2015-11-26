5c7bed80a3100f80eff56b0af8a8ca1d795e5479
Gabriela Vital <gabisinha0107@hotmail.com>
2015-11-25 22:21:36 -0200

    .

--- MenuW.java
+++ MenuW.java
@@ -1,5 +1,3 @@
-package geniusGame;
-
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
@@ -24,6 +22,7 @@ import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
 
 import com.jgoodies.forms.factories.DefaultComponentFactory;
+import javax.swing.JTextArea;
 
 public class MenuW extends JFrame {
 
@@ -50,27 +49,10 @@ public class MenuW extends JFrame {
 	private JButton btnVoltar_4;
 
 	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MenuW frame = new MenuW();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
+	 * Criando o Frame
 	 */
 	public MenuW() {
 		setTitle("GeniusGame");
-		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 381, 355);
 		telaMenuW = new JPanel();
@@ -78,40 +60,42 @@ public class MenuW extends JFrame {
 		setContentPane(telaMenuW);
 		telaMenuW.setLayout(null);
 
-		JLabel lblGeniusGame = DefaultComponentFactory.getInstance()
-				.createLabel("Genius Game");
+		JLabel lblGeniusGame = new JLabel("Genius Game");
 		lblGeniusGame.setFont(new Font("Bauhaus 93", Font.PLAIN, 26));
-		lblGeniusGame.setBounds(93, 11, 158, 34);
+		lblGeniusGame.setBounds(109, 11, 158, 34);
 		telaMenuW.add(lblGeniusGame);
 		
 		
-
+		// criando os botões do menu
 		btnJogar = new JButton("Jogar");
 		btnJogar.addActionListener(new ActionListener() {
+			// adicionando evento
 			public void actionPerformed(ActionEvent e) {
+				// ao clicar no botão JOGAR ele desativa os outros botões deixando ativo apenas o menu selecionado e a opção sair
 				btnOpes.setEnabled(false);
 				btnRanking.setEnabled(false);
 				btnAjuda.setEnabled(false);
 				
-				
+				// adiciona um novo JPanel com as funções do menu selecionado
 				moduloJogo = new Panel();
 				moduloJogo.setBounds(121, 75, 172, 160);
 				telaMenuW.add(moduloJogo);
 				moduloJogo.setLayout(null);
-
-				btnRepitaASequncia = new JButton("Repita a Sequ\u00EAncia");
+				
+				// botões para selecionar módulo jogo
+				btnRepitaASequncia = new JButton("Repita a Sequência");
 				btnRepitaASequncia.setFont(new Font("Berlin Sans FB", Font.PLAIN, 12));
 				btnRepitaASequncia.setBounds(10, 11, 152, 23);
 				moduloJogo.add(btnRepitaASequncia);
 
-				btnCrieSuaSequncia = new JButton("Crie Sua Sequ\u00EAncia");
+				btnCrieSuaSequncia = new JButton("Crie sua Sequência");
 				btnCrieSuaSequncia.setFont(new Font("Berlin Sans FB", Font.PLAIN, 12));
 				btnCrieSuaSequncia.setBounds(10, 45, 152, 23);
 				moduloJogo.add(btnCrieSuaSequncia);
 
-				btnSequnciaComTempo = new JButton("Sequ\u00EAncia com Tempo");
+				btnSequnciaComTempo = new JButton("Sequência com Tempo");
 				/***********************************************************************
-				 * pensar como tirar o panel com os m�dulos de jogo
+				 * pensar como tirar o panel com os módulos de jogo
 				 * *********************************************************************
 				btnSequnciaComTempo.addActionListener(new ActionListener(){
 					public void actionPerformed(ActionEvent e) {
@@ -158,6 +142,7 @@ public class MenuW extends JFrame {
 				btnSequnciaComTempo.setBounds(10, 79, 152, 23);
 				moduloJogo.add(btnSequnciaComTempo);
 				
+				// botão para remover JPanel e ativar os botões desativados anteriormente
 				btnVoltar = new JButton("Voltar");
 				btnVoltar.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
@@ -175,19 +160,23 @@ public class MenuW extends JFrame {
 		btnJogar.setFont(new Font("Berlin Sans FB", Font.PLAIN, 12));
 		btnJogar.setBounds(10, 76, 89, 23);
 		telaMenuW.add(btnJogar);
-
-		btnOpes = new JButton("Op\u00E7\u00F5es");
+		
+		// botão para criar JPanel de opções
+		btnOpes = new JButton("Opções");
 		btnOpes.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				// desativa os outros botões
 				btnJogar.setEnabled(false);
 				btnRanking.setEnabled(false);
 				btnAjuda.setEnabled(false);
 				
+				// adiciona JPanel de opções
 				panelOpcoes = new Panel();
 				panelOpcoes.setBounds(121, 75, 235, 194);
 				telaMenuW.add(panelOpcoes);
 				panelOpcoes.setLayout(null);
 				
+				// mudar nome do jogador
 				JLabel lblPlayer = new JLabel("Player:");
 				lblPlayer.setFont(new Font("Berlin Sans FB", Font.PLAIN, 12));
 				lblPlayer.setBounds(10, 11, 40, 14);
@@ -198,10 +187,29 @@ public class MenuW extends JFrame {
 				panelOpcoes.add(nomeJogador);
 				nomeJogador.setColumns(10);
 				
+				// ligar/desligar e mudar volume dos sons
 				JLabel lblMsica = new JLabel("Sons");
 				lblMsica.setFont(new Font("Berlin Sans FB", Font.PLAIN, 12));
 				lblMsica.setBounds(10, 44, 40, 14);
 				panelOpcoes.add(lblMsica);
+				
+				final JToggleButton tglbtnSom = new JToggleButton("Ligado");
+				tglbtnSom.setSelected(true);
+				tglbtnSom.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if(tglbtnSom.isSelected()){
+							tglbtnSom.setBackground(Color.lightGray);
+							tglbtnSom.setForeground(Color.black);
+							tglbtnSom.setText("Ligado");
+						}else{
+							tglbtnSom.setBackground(Color.darkGray);
+							tglbtnSom.setForeground(Color.white);
+							tglbtnSom.setText("Desligado");
+						}
+					}
+				});
+				tglbtnSom.setBounds(40, 57, 121, 23);
+				panelOpcoes.add(tglbtnSom);
 				
 				JSlider slider = new JSlider();
 				slider.setBounds(56, 103, 169, 14);
@@ -233,24 +241,7 @@ public class MenuW extends JFrame {
 				separator_3.setBounds(8, 130, 220, 2);
 				panelOpcoes.add(separator_3);
 				
-				final JToggleButton tglbtnSom = new JToggleButton("Ligado");
-				tglbtnSom.setSelected(true);
-				tglbtnSom.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						if(tglbtnSom.isSelected()){
-							tglbtnSom.setBackground(Color.lightGray);
-							tglbtnSom.setForeground(Color.black);
-							tglbtnSom.setText("Ligado");
-						}else{
-							tglbtnSom.setBackground(Color.darkGray);
-							tglbtnSom.setForeground(Color.white);
-							tglbtnSom.setText("Desligado");
-						}
-					}
-				});
-				tglbtnSom.setBounds(40, 57, 121, 23);
-				panelOpcoes.add(tglbtnSom);
-				
+				// botão para remover JPanel e ativar os botões desativados anteriormente
 				JButton btnVoltar_1 = new JButton("Voltar");				
 				btnVoltar_1.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent arg0) {
@@ -268,25 +259,139 @@ public class MenuW extends JFrame {
 		btnOpes.setFont(new Font("Berlin Sans FB", Font.PLAIN, 12));
 		btnOpes.setBounds(10, 110, 89, 23);
 		telaMenuW.add(btnOpes);
-
+		
+		// JPanel para ranking
 		btnRanking = new JButton("Ranking");
 		btnRanking.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				// desativa os outros botões
 				btnJogar.setEnabled(false);
 				btnAjuda.setEnabled(false);
 				btnOpes.setEnabled(false);
 				
+				// adiciona JPanel de ranking
 				final JPanel panelRanking = new JPanel();
-				panelRanking.setBounds(121, 76, 244, 199);
+				panelRanking.setBounds(121, 56, 244, 259);
 				telaMenuW.add(panelRanking);
 				panelRanking.setLayout(null);
 				
+				// titulo
 				JLabel lblRanking = new JLabel("Ranking");
-				lblRanking.setBounds(10, 11, 46, 14);
+				lblRanking.setFont(new Font("Berlin Sans FB Demi", Font.PLAIN, 13));
+				lblRanking.setBounds(1, 1, 55, 14);
 				panelRanking.add(lblRanking);
 				
+				// label para indicar tipo do jogo
+				JLabel lblTipoDoJogo = new JLabel("Tipo do jogo");
+				lblTipoDoJogo.setFont(new Font("Berlin Sans FB", Font.PLAIN, 12));
+				lblTipoDoJogo.setBounds(41, 26, 62, 14);
+				panelRanking.add(lblTipoDoJogo);
+				
+				// alterar nome aqui conforme o modulo selecionado
+				JLabel lblNomeTipoJogoMuda = new JLabel("TipoJogoNome");
+				lblNomeTipoJogoMuda.setForeground(new Color(0, 0, 255));
+				lblNomeTipoJogoMuda.setFont(new Font("Berlin Sans FB Demi", Font.PLAIN, 12));
+				lblNomeTipoJogoMuda.setBounds(120, 26, 93, 14);
+				panelRanking.add(lblNomeTipoJogoMuda);
+				
+				// separando as informações com uma linha
+				JSeparator separator_1 = new JSeparator();
+				separator_1.setBounds(8, 45, 227, 2);
+				panelRanking.add(separator_1);
+				
+				// label Jogador
+				JLabel lblJogador = new JLabel("Jogador");
+				lblJogador.setFont(new Font("Berlin Sans FB", Font.PLAIN, 12));
+				lblJogador.setBounds(32, 62, 46, 14);
+				panelRanking.add(lblJogador);
+				
+				// campo para exibir nome jogador
+				JTextArea txtrJogador = new JTextArea();
+				txtrJogador.setEditable(false);
+				txtrJogador.setFont(new Font("Arial Narrow", Font.PLAIN, 13));
+				txtrJogador.setText("-----------");
+				txtrJogador.setBounds(20, 79, 106, 19);
+				panelRanking.add(txtrJogador);
+				
+				// labl pontuação
+				JLabel lblPontuao = new JLabel("Pontuação");
+				lblPontuao.setFont(new Font("Berlin Sans FB", Font.PLAIN, 12));
+				lblPontuao.setBounds(140, 62, 57, 14);
+				panelRanking.add(lblPontuao);
+				
+				// campo para exibir pontuação do jogador
+				JTextArea textArea_1 = new JTextArea();
+				textArea_1.setText("-------------");
+				textArea_1.setEditable(false);
+				textArea_1.setFont(new Font("Arial Narrow", Font.PLAIN, 13));
+				textArea_1.setBounds(137, 79, 76, 19);
+				panelRanking.add(textArea_1);
+				
+				// label para exibir total tentativas
+				JLabel lblTentativas = new JLabel("Total Tentativas");
+				lblTentativas.setFont(new Font("Berlin Sans FB", Font.PLAIN, 12));
+				lblTentativas.setBounds(8, 109, 81, 14);
+				panelRanking.add(lblTentativas);
+				
+				// campo para exibir total tentativas
+				JTextArea txtNroTotTenta = new JTextArea();
+				txtNroTotTenta.setFont(new Font("Arial Narrow", Font.PLAIN, 13));
+				txtNroTotTenta.setEditable(false);
+				txtNroTotTenta.setText("---------------");
+				txtNroTotTenta.setBounds(99, 104, 114, 19);
+				panelRanking.add(txtNroTotTenta);
+				
+				// label para tentativas corretas
+				JLabel lblCorretas = new JLabel("Corretas");
+				lblCorretas.setForeground(new Color(0, 128, 0));
+				lblCorretas.setFont(new Font("Berlin Sans FB Demi", Font.PLAIN, 12));
+				lblCorretas.setBounds(64, 134, 54, 14);
+				panelRanking.add(lblCorretas);
+				
+				// campo para exibir tentativas corretas
+				JTextArea txtrNroTentaC = new JTextArea();
+				txtrNroTentaC.setEditable(false);
+				txtrNroTentaC.setFont(new Font("Arial Narrow", Font.PLAIN, 13));
+				txtrNroTentaC.setText("---------------");
+				txtrNroTentaC.setBounds(120, 130, 114, 19);
+				panelRanking.add(txtrNroTentaC);
+				
+				// label para tentativas erradas
+				JLabel lblErradas = new JLabel("Erradas");
+				lblErradas.setForeground(new Color(220, 20, 60));
+				lblErradas.setFont(new Font("Berlin Sans FB Demi", Font.PLAIN, 12));
+				lblErradas.setBounds(68, 159, 46, 14);
+				panelRanking.add(lblErradas);
+				
+				// campo para exibir tentativas erradas
+				JTextArea txtrNrotentae = new JTextArea();
+				txtrNrotentae.setEditable(false);
+				txtrNrotentae.setFont(new Font("Arial Narrow", Font.PLAIN, 13));
+				txtrNrotentae.setText("---------------");
+				txtrNrotentae.setBounds(120, 155, 114, 19);
+				panelRanking.add(txtrNrotentae);
+				
+				// botão para exibir resultado de outro jogador cadastrado
+				JButton btnTrocarJogador = new JButton("Trocar Jogador");
+				btnTrocarJogador.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						//se nao existe nenhum jogador cadastrado avisar que nao existe nenhum resultado ainda
+						
+					}
+				});
+				btnTrocarJogador.setFont(new Font("Berlin Sans FB", Font.PLAIN, 12));
+				btnTrocarJogador.setBounds(11, 191, 108, 23);
+				panelRanking.add(btnTrocarJogador);
+				
+				// alterar tipo do jogo pra ver o resultado especifico
+				JButton btnMdulos = new JButton("Tipo Jogo");
+				btnMdulos.setFont(new Font("Berlin Sans FB", Font.PLAIN, 12));
+				btnMdulos.setBounds(124, 191, 89, 23);
+				panelRanking.add(btnMdulos);
+				
+				// botão para remover JPanel e ativar os botões desativados anteriormente
 				btnVoltar_3 = new JButton("Voltar");
-				btnVoltar_3.setBounds(85, 165, 69, 23);
+				btnVoltar_3.setBounds(88, 225, 69, 23);
 				panelRanking.add(btnVoltar_3);
 				btnVoltar_3.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
@@ -330,63 +435,67 @@ public class MenuW extends JFrame {
 		btnRanking.setFont(new Font("Berlin Sans FB", Font.PLAIN, 12));
 		btnRanking.setBounds(10, 144, 89, 23);
 		telaMenuW.add(btnRanking);
-
+		
+		// JPanel para ajuda
 		btnAjuda = new JButton("Ajuda");
 		btnAjuda.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				// desativa os outros botões
 				btnJogar.setEnabled(false);
 				btnRanking.setEnabled(false);
 				btnOpes.setEnabled(false);
 				
+				// adiciona JPanel de ajuda
 				panelAjuda = new Panel();
 				panelAjuda.setBounds(117, 76, 238, 230);
 				telaMenuW.add(panelAjuda);
 				panelAjuda.setLayout(null);
 				
+				// texto para ajuda
 				JTextPane textoAjuda = new JTextPane();
 				textoAjuda.setFont(new Font("Berlin Sans FB", Font.PLAIN, 12));
 				textoAjuda.setBackground(UIManager.getColor("Panel.background"));
-				textoAjuda.setText("O objetivo no jogo \u00E9 acompanhar e repetir sem errar a sequ\u00EAncia de luzes e sons durante o tempo que puder... Se voc\u00EA puder, \u00E9 claro... :)");
+				textoAjuda.setText("O objetivo no jogo é acompanhar e usando o mouse, repetir sem errar a sequência de luzes e sons durante o tempo que puder... Se você puder, é claro... :)");
 				textoAjuda.setBounds(0, 0, 238, 62);
 				panelAjuda.add(textoAjuda);
 				
 				JTextPane txtpnRepitaASeq = new JTextPane();
 				txtpnRepitaASeq.setBackground(UIManager.getColor("Panel.background"));
 				txtpnRepitaASeq.setFont(new Font("Berlin Sans FB Demi", Font.PLAIN, 12));
-				txtpnRepitaASeq.setText("Repita a sequ\u00EAncia");
+				txtpnRepitaASeq.setText("Repita a sequência");
 				txtpnRepitaASeq.setBounds(0, 65, 116, 20);
 				panelAjuda.add(txtpnRepitaASeq);
 				
 				JTextPane txtpnRepSeqExplicacao = new JTextPane();
 				txtpnRepSeqExplicacao.setBackground(UIManager.getColor("Panel.background"));
 				txtpnRepSeqExplicacao.setFont(new Font("Berlin Sans FB", Font.PLAIN, 12));
-				txtpnRepSeqExplicacao.setText("Tente repetir a sequ\u00EAncia criada pelo jogo");
+				txtpnRepSeqExplicacao.setText("Tente repetir a sequência criada pelo jogo");
 				txtpnRepSeqExplicacao.setBounds(10, 80, 218, 20);
 				panelAjuda.add(txtpnRepSeqExplicacao);
 				
 				JTextPane txtpnCrieSuaSeq = new JTextPane();
-				txtpnCrieSuaSeq.setText("Crie sua sequ\u00EAncia");
+				txtpnCrieSuaSeq.setText("Crie sua sequência");
 				txtpnCrieSuaSeq.setFont(new Font("Berlin Sans FB Demi", Font.PLAIN, 12));
 				txtpnCrieSuaSeq.setBackground(SystemColor.menu);
 				txtpnCrieSuaSeq.setBounds(0, 108, 128, 20);
 				panelAjuda.add(txtpnCrieSuaSeq);
 				
 				JTextPane txtpnCrieSeqExplicacao = new JTextPane();
-				txtpnCrieSeqExplicacao.setText("Crie uma sequ\u00EAncia e tente repeti-la");
+				txtpnCrieSeqExplicacao.setText("Crie uma sequência e tente repeti-la");
 				txtpnCrieSeqExplicacao.setFont(new Font("Berlin Sans FB", Font.PLAIN, 12));
 				txtpnCrieSeqExplicacao.setBackground(SystemColor.menu);
 				txtpnCrieSeqExplicacao.setBounds(10, 123, 218, 20);
 				panelAjuda.add(txtpnCrieSeqExplicacao);
 				
 				JTextPane txtpnSeqComTempo = new JTextPane();
-				txtpnSeqComTempo.setText("Sequ\u00EAncia com Tempo");
+				txtpnSeqComTempo.setText("Sequência com Tempo");
 				txtpnSeqComTempo.setFont(new Font("Berlin Sans FB Demi", Font.PLAIN, 12));
 				txtpnSeqComTempo.setBackground(SystemColor.menu);
 				txtpnSeqComTempo.setBounds(0, 150, 134, 20);
 				panelAjuda.add(txtpnSeqComTempo);
 				
 				JTextPane txtpnSeqTempExplicacao = new JTextPane();
-				txtpnSeqTempExplicacao.setText("Pense r\u00E1pido pois voc\u00EA tem um tempo para jogar.");
+				txtpnSeqTempExplicacao.setText("Pense rápido pois você tem um tempo para jogar.");
 				txtpnSeqTempExplicacao.setFont(new Font("Berlin Sans FB", Font.PLAIN, 12));
 				txtpnSeqTempExplicacao.setBackground(SystemColor.menu);
 				txtpnSeqTempExplicacao.setBounds(10, 167, 218, 34);
@@ -400,6 +509,7 @@ public class MenuW extends JFrame {
 				separator_2.setBounds(39, 146, 141, 2);
 				panelAjuda.add(separator_2);
 				
+				// botão para remover JPanel e ativar os botões desativados anteriormente
 				JButton btnVoltar_2 = new JButton("Voltar");
 				btnVoltar_2.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
@@ -417,10 +527,12 @@ public class MenuW extends JFrame {
 		btnAjuda.setFont(new Font("Berlin Sans FB", Font.PLAIN, 12));
 		btnAjuda.setBounds(10, 178, 89, 23);
 		telaMenuW.add(btnAjuda);
-
+		
+		// botao para fechar o frame e sair
 		btnSair = new JButton("Sair");
 		btnSair.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				// fechando a aplicação
 				System.exit(0);
 			}
 		});
@@ -433,23 +545,21 @@ public class MenuW extends JFrame {
 		separator.setForeground(Color.DARK_GRAY);
 		separator.setBounds(109, 70, 2, 205);
 		telaMenuW.add(separator);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		//--------------------------------------------------------------------------
-		
+	}
+	
+	/**
+	 * Iniciando a aplicação
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuW frame = new MenuW();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 }
