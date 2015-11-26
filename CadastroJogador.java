1cdfecc2850d5a91395bed30a92a04e5a78c4146
Gabriela Vital <gabisinha0107@hotmail.com>
2015-11-26 19:48:55 -0200

    Create CadastroJogador.java

---
+++ CadastroJogador.java
@@ -0,0 +1,57 @@
+package geniusGame;
+
+/*******************************************
+ * Tela inicial para cadastro do jogador
+ *******************************************/
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+public class CadastroJogador extends JFrame implements ActionListener {
+	private static final long serialVersionUID = 1L;
+
+	private JButton botaoEnviar = new JButton("Enviar");
+	private JTextField campoNome = new JTextField(25);
+	private JLabel labelCampoNome = new JLabel("Nome:");
+
+	public CadastroJogador() {
+		super("Genius - Cadastro");
+		this.setSize(400, 100);
+		this.setVisible(true);
+		this.setLayout(new FlowLayout());
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setResizable(false);
+
+		this.add(labelCampoNome);
+		this.add(campoNome);
+		this.add(botaoEnviar);
+
+		botaoEnviar.addActionListener(this);
+
+	}
+	
+
+
+
+	public void actionPerformed(ActionEvent evento) {
+		if (evento.getSource() == botaoEnviar) {
+			ConfigUser.nomeUsuario = campoNome.getText();
+			if( ConfigUser.nomeUsuario != null && !ConfigUser.nomeUsuario.isEmpty()){
+			new Menu();
+			this.setVisible(false);
+			this.dispose();
+		} else {
+			JOptionPane.showMessageDialog(null, "Digite um nome de usuario");
+		}
+		}
+	}
+
+}
